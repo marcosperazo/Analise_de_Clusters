36e0331c05698857aeeee0bb952f7c75e9254040
Marcos Perazo <marcosperazo@icloud.com>
2025-08-25 01:22:28 -0300

    continuação da segunda parte

--- Pumpkin.R
+++ Pumpkin.R
@@ -39,4 +39,30 @@ head(dados)        # Mostra as primeiras linhas
 str(dados)         # Estrutura das variáveis
 summary(dados)     # Estatísticas básicas
 
+#Normalização dos dados
 dados_padronizados <- scale(dados)
+
+set.seed(223)
+# Método do cotovelo (WSS)
+fviz_nbclust(dados_padronizados, kmeans, method = "wss") +
+  ggplot2::geom_vline(xintercept = 2, linetype = 2, color = "red") +
+  ggplot2::labs(title = "Elbow Method (WSS)")
+
+
+# Método da silhueta
+fviz_nbclust(dados_padronizados, kmeans, method = "silhouette") +
+  ggplot2::labs(title = "Silhouette Method")
+
+
+# executar kmeans (nstart alto para evitar mínimos locais)
+set.seed(223)
+km_res <- kmeans(dados_padronizados, centers = 2, nstart = 25)
+print(km_res)
+
+# Visualização
+fviz_cluster(km_res, data = dados_padronizados, ellipse.type = "convex",
+             ggtheme = theme_minimal(), main = "K-means (k=2)")
+
+
+# Comparar com classes reais
+print(table(Cluster = km_res$cluster, TrueClass = iris$Species))
